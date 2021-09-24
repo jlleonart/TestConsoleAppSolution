0a2386cb4720439b81bc5315b2a3b0d8209fa39e
Joel Lleonart <jplleonart@gmail.com>
2021-09-24 10:03:25 -0400

    Inital commit

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("To the C# World");
+
+            int NumberOfStudents = 17;
+            var TotalInClass = NumberOfStudents + 2;
+            Console.WriteLine($"Number of Students is {NumberOfStudents}, total is {TotalInClass}");
+            var myName = "Joel";
+            Console.WriteLine($"{myName} is one of the {NumberOfStudents} students");
+            Console.WriteLine($"{myName} is one of the {NumberOfStudents}students");
+        }
+    }
+}
